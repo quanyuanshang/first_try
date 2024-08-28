54f7be8e496af7d161955c5af57bf34b2d9573c9
quanquan <3252983692@qq.com>
2024-08-28 23:01:40 +0800

    第一次提交

---
+++ shedule.py
@@ -0,0 +1,26 @@
+
+
+
+计算机概论
+计算机系统概论
+
+一站式学习C编程
+https://book.douban.com/subject/6025290/
+
+Linux 
+鸟哥的Linux私房菜 基础学习篇 第四版
+
+python
+MIT 6.006: Introduction to Algorithms
+
+C/C++
+Accelerated C++ 
+
+
+算法
+
+刷题
+leetcode
+代码随想录
+https://www.programmercarl.com/
+
